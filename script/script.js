0c9d0bf771bcc9eab41e47e994bff0782ec62771
Ranieeery <raniery2003@hotmail.com>
2023-01-27 16:12:18 -0300

    [fix] - fix the bug of the button and add the function

--- script/script.js
+++ script/script.js
@@ -1,29 +1,65 @@
+// As "chaves" de criptografia que utilizaremos são:
+// A letra "e" é convertida para "enter"
+// A letra "i" é convertida para "imes"
+// A letra "a" é convertida para "ai"
+// A letra "o" é convertida para "ober"
+// A letra "u" é convertida para "ufat"
+
+const textareaInput = document.querySelector("#textarea");
+const textareaMsg = document.querySelector("#textarea--show");
+
 const buttonCrip = document.querySelector(".button--crip");
 const buttonDescrip = document.querySelector(".button--descrip");
-let textMsg = document.getElementsByClassName("msg--text");
-let textareaInput;
+const buttonCopy = document.querySelector(".button--copy");
 
-const descript = ["e", "i", "a", "o", "u"];
-const cript = ["enter", "imes", "ai", "ober", "ufat"];
+function criptografar(stringTexto) {
+  let matriz = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringTexto = stringTexto.toLowerCase();
+
+  for (let i = 0; i < matriz.length; i++) {
+    if (stringTexto.includes(matriz[i][0])) {
+      stringTexto = stringTexto.replaceAll(matriz[i][0], matriz[i][1]);
+    }
+  }
+  return stringTexto;
+}
 
 buttonCrip.addEventListener("click", () => {
-    textareaInput = document.querySelector("#textarea").value;
-    cripText(textareaInput);
+  const textoCrip = criptografar(textareaInput.value);
+  textareaMsg.value = textoCrip;
+  textareaInput.value = "";
 });
+
+function descriptografar(stringTexto) {
+  let matriz = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringTexto = stringTexto.toLowerCase();
+
+  for (let i = 0; i < matriz.length; i++) {
+    if (stringTexto.includes(matriz[i][1])) {
+      stringTexto = stringTexto.replaceAll(matriz[i][1], matriz[i][0]);
+    }
+  }
+  return stringTexto;
+}
 
 buttonDescrip.addEventListener("click", () => {
-    textareaInput = document.querySelector("#textarea").value;
-    descripText(textareaInput);
+  const textoDescrip = descriptografar(textareaInput.value);
+  textareaMsg.value = textoDescrip;
+  textareaInput.value = "";
 });
 
-function cripText(text) {
-    for (let i = 0; i < descript.length; i++)
-        text = text.replaceAll(descript[i], cript[i]);
-    console.log(text);
-}
-
-function descripText(text) {
-    for (let i = 0; i < cript.length; i++)
-        text = text.replaceAll(cript[i], descript[i]);
-    console.log(text);
-}
+buttonCopy.addEventListener("click", () => {
+  navigator.clipboard.writeText(textareaMsg.value);
+});
